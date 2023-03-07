079c746ab96b1895ae68a9b2bc722d512bee3f34
Werner Oosthuizen <werner.oosthuizen@doxim.com>
2023-03-07 15:12:22 +0200

    pushed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import Footer from './components/footer';
 import Header from './components/header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
